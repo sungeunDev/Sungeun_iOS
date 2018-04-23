863c717ad312cd496d4d423fc416cbb94720bc0b
sungeunDev <starfg62@gmail.com>
2018-04-23 23:47:27 +0900

    update iOS School files

---
+++ Practice/180319 AutoLayout/facebookLoginStudy/facebookLoginStudy/testViewController.swift
@@ -0,0 +1,36 @@
+//
+//  testViewController.swift
+//  facebookLoginStudy
+//
+//  Created by sungnni on 2018. 4. 11..
+//  Copyright © 2018년 SsungNni. All rights reserved.
+//
+
+import UIKit
+import FBSDK
+
+class testViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
