7a1d7c015f37adfd6b00fecc31a641ec92cf0a4c
sungeunDev <starfg62@gmail.com>
2019-03-12 14:39:05 +0900

    Update RxSwift Study

---
+++ Study/Test/BubbleTabTest/BubbleTabTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BubbleTabTest
+//
+//  Created by Ari on 18/02/2019.
+//  Copyright © 2019 ssungnni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Study/Test/RSwiftStudy/RSwiftStudy/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RSwiftStudy
+//
+//  Created by Ari on 14/02/2019.
+//  Copyright © 2019 ssungnni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let birdImage: UIImage = R.storyboard.main()
+    }
+
+
+}
+
